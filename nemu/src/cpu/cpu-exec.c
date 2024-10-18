e1e2d482eaf41759ae198ce3936c869397ca80a8
tracer-ics2024 <tracer@njuics.org>
2024-10-19 04:44:58 +0800

    >  compile NEMU SA24218129 ysxAshore Linux ysxashore-pc 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:44:58 up 37 min,  1 user,  load average: 0.05, 0.10, 0.15

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,7 +46,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
     IFDEF(CONFIG_ITRACE, puts(_this->logbuf));
   }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-  checkWatchesStatus();
+  IFDEF(CONFIG_WATCHPOINT, checkWatchesStatus());
 }
 
 static void exec_once(Decode *s, vaddr_t pc)
