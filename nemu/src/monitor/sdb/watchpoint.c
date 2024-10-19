54600cd08a34bf66e6c4320218e81b0b65468c67
tracer-ics2024 <tracer@njuics.org>
2024-10-19 20:35:51 +0800

    >  compile NEMU SA24218129 ysxAshore Linux ysxashore-pc 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:51 up  2:02,  1 user,  load average: 0.84, 0.29, 0.21

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -115,10 +115,7 @@ void createAWatch(char *args)
     awp->val = val;
     awp->next = head;
     head = awp;
-    if (strcpy(awp->expression, "$pc"))
-      printf("The %d watch has created,%s = %lx\n", awp->NO, awp->expression, awp->val);
-    else
-      printf("The %d watch has created,%s = %lu\n", awp->NO, awp->expression, awp->val);
+    printf("The %d watch has created,%s = %lx\n", awp->NO, awp->expression, awp->val);
   }
   else
     printf("The expression %s isn't solvable\n", args);
@@ -132,10 +129,8 @@ void checkWatchesStatus()
     uint64_t nowVal = expr(p->expression, &sign);
     if (sign && nowVal != p->val)
     {
-      if (strcmp(p->expression, "$pc") == 0)
-        printf("The %d watch watches the expression %s has changed,from %lx to %lx\n", p->NO, p->expression, p->val, nowVal);
-      else
-        printf("The %d watch watches the expression %s has changed,from %lu to %lu\n", p->NO, p->expression, p->val, nowVal);
+
+      printf("The %d watch watches the expression %s has changed,from %lx to %lx\n", p->NO, p->expression, p->val, nowVal);
       p->val = nowVal;
       nemu_state.state = NEMU_STOP;
     }
