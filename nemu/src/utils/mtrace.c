f0794927bd2aacdf183b861be219817c9165e26c
ysxAshore <19819660125@163.com>
2024-10-31 19:40:24 +0800

    complete the mtrace

--- nemu/src/utils/mtrace.c
+++ nemu/src/utils/mtrace.c
@@ -1,10 +1,10 @@
 #include <common.h>
 void display_pread(paddr_t addr, int len)
 {
-    printf("pread at " FMT_PADDR " len=%d\n", addr, len);
+    printf("paddr_read  at " FMT_PADDR " len=%d\n", addr, len);
 }
 
 void display_pwrite(paddr_t addr, int len, word_t data)
 {
-    printf("pwrite at " FMT_PADDR " len=%d, data=" FMT_WORD "\n", addr, len, data);
+    printf("paddr_write at " FMT_PADDR " len=%d, data=" FMT_WORD "\n", addr, len, data);
 }
