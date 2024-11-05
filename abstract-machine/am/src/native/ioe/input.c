8f09f86bd7b3334d0bb524574781d902e51ba3d5
ysxAshore <19819660125@163.com>
2024-11-05 10:34:43 +0800

    complete the keyboard ioe

--- abstract-machine/am/src/native/ioe/input.c
+++ abstract-machine/am/src/native/ioe/input.c
@@ -10,49 +10,58 @@ static SDL_mutex *key_queue_lock = NULL;
 
 #define XX(k) [SDL_SCANCODE_##k] = AM_KEY_##k,
 static int keymap[256] = {
-  AM_KEYS(XX)
-};
+    AM_KEYS(XX)};
 
-static int event_thread(void *args) {
+static int event_thread(void *args)
+{
   SDL_Event event;
-  while (1) {
+  while (1)
+  {
     SDL_WaitEvent(&event);
-    switch (event.type) {
-      case SDL_QUIT: halt(0);
-      case SDL_KEYDOWN:
-      case SDL_KEYUP: {
-        SDL_Keysym k = event.key.keysym;
-        int keydown = event.key.type == SDL_KEYDOWN;
-        int scancode = k.scancode;
-        if (keymap[scancode] != 0) {
-          int am_code = keymap[scancode] | (keydown ? KEYDOWN_MASK : 0);
-          SDL_LockMutex(key_queue_lock);
-          key_queue[key_r] = am_code;
-          key_r = (key_r + 1) % KEY_QUEUE_LEN;
-          SDL_UnlockMutex(key_queue_lock);
-          void __am_send_kbd_intr();
-          __am_send_kbd_intr();
-        }
-        break;
+    switch (event.type)
+    {
+    case SDL_QUIT:
+      halt(0);
+    case SDL_KEYDOWN:
+    case SDL_KEYUP:
+    {
+      SDL_Keysym k = event.key.keysym;
+      int keydown = event.key.type == SDL_KEYDOWN;
+      int scancode = k.scancode;
+      if (keymap[scancode] != 0)
+      {
+        int am_code = keymap[scancode] | (keydown ? KEYDOWN_MASK : 0);
+        SDL_LockMutex(key_queue_lock);
+        key_queue[key_r] = am_code;
+        key_r = (key_r + 1) % KEY_QUEUE_LEN;
+        SDL_UnlockMutex(key_queue_lock);
+        void __am_send_kbd_intr();
+        __am_send_kbd_intr();
       }
+      break;
+    }
     }
   }
 }
 
-void __am_input_init() {
+void __am_input_init()
+{
   key_queue_lock = SDL_CreateMutex();
   SDL_CreateThread(event_thread, "event thread", NULL);
 }
 
-void __am_input_config(AM_INPUT_CONFIG_T *cfg) {
+void __am_input_config(AM_INPUT_CONFIG_T *cfg)
+{
   cfg->present = true;
 }
 
-void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd)
+{
   int k = AM_KEY_NONE;
 
   SDL_LockMutex(key_queue_lock);
-  if (key_f != key_r) {
+  if (key_f != key_r)
+  {
     k = key_queue[key_f];
     key_f = (key_f + 1) % KEY_QUEUE_LEN;
   }
