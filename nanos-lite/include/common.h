82b5889fe2ffc01a77b1e39a60ec06ec0b202cc4
ysxAshore <19819660125@163.com>
2024-11-09 20:22:14 +0800

    passed the codes with addition of HAS_CTE macro,including init_irq and yield

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -2,10 +2,10 @@
 #define __COMMON_H__
 
 /* Uncomment these macros to enable corresponding functionality. */
-//#define HAS_CTE
-//#define HAS_VME
-//#define MULTIPROGRAM
-//#define TIME_SHARING
+#define HAS_CTE
+// #define HAS_VME
+// #define MULTIPROGRAM
+// #define TIME_SHARING
 
 #include <am.h>
 #include <klib.h>
