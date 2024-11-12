40b1a5ac2bb596f272c28ca0f02264b3ffd0e20c
ysxAshore <19819660125@163.com>
2024-11-12 21:09:12 +0800

    implenmented the syscall trace

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,11 +9,17 @@ void do_syscall(Context *c)
   {
   case 0: // SYS_exit syscall
     c->GPRx = 0;
+#ifdef CONFIG_STRACE
+    Log("Thers is a SYS_exit syscall,the arguments is %p,%p,%p,the return value is %p", c->GPR2, c->GPR3, c->GPR4, c->GPRx);
+#endif
     halt(c->GPR2);
     break;
   case 1: // SYS_yield　syscall
     yield();
     c->GPRx = 0;
+#ifdef CONFIG_STRACE
+    Log("Thers is a SYS_yield syscall,the arguments is %p,%p,%p,the return value is %p", c->GPR2, c->GPR3, c->GPR4, c->GPRx);
+#endif
     break;
   default:
     panic("Unhandled syscall ID = %d", a[0]);
