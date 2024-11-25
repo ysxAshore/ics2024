49ec6a14c7ff818f064b326209954043e5bb3051
ysxAshore <19819660125@163.com>
2024-11-25 22:24:29 +0800

    implenmented the SDL_LoadWAV,SDL_FreeWAV and SDL_MixAudio function to run sdlbird with audio

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -1,6 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
 #include <stdlib.h>
+#include <assert.h>
 
 static uint32_t gap;
 static int freq;
@@ -72,15 +73,57 @@ void SDL_PauseAudio(int pause_on)
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume)
 {
+  if (!dst || !src || len < 0 || volume < 0 || volume > SDL_MIX_MAXVOLUME)
+    return;
+  for (size_t i = 0; i < len; ++i)
+  {
+    int mixed_sample = dst[i] + src[i] * volume / SDL_MIX_MAXVOLUME;
+    if (mixed_sample > 255)
+      mixed_sample = 255;
+    else if (mixed_sample < 0)
+      mixed_sample = 0;
+
+    dst[i] = (uint8_t)mixed_sample;
+  }
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len)
 {
-  return NULL;
+  FILE *fp = fopen(file, "rb");
+
+  // 读取RIFF chunk
+  fseek(fp, 0, SEEK_SET);
+  uint32_t riff_id, riff_type;
+  fread(&riff_id, 4, 1, fp);
+  assert(riff_id == 0x46464952); // big endian
+  fseek(fp, 8, SEEK_SET);
+  fread(&riff_type, 4, 1, fp);
+  assert(riff_type == 0x45564157); // big endian
+
+  // 读取Format chunk
+  uint32_t format_id;
+  fread(&format_id, 4, 1, fp);
+  assert(format_id == 0x20746D66); // big endian
+  fseek(fp, 0x16, SEEK_SET);
+  fread(&spec->channels, 2, 1, fp);
+  fread(&spec->freq, 4, 1, fp);
+  fseek(fp, 0x22, SEEK_SET);
+  fread(&spec->samples, 2, 1, fp);
+
+  // 读取Data chunk
+  uint32_t data_id;
+  fread(&data_id, 4, 1, fp);
+  assert(data_id == 0x61746164); // big endian
+  fread(audio_len, 4, 1, fp);
+  *audio_buf = (uint8_t *)malloc(*audio_len);
+  fread(*audio_buf, *audio_len, 1, fp);
+
+  return spec;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf)
 {
+  free(audio_buf);
 }
 
 void SDL_LockAudio()
