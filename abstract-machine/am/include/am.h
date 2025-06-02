93790a18ce8ab188225681425afd6c8607bd2ddf
ysxAshore <19819660125@163.com>
2025-06-02 21:27:55 +0800

    [add]implenmented the batch mode which running abstract-machine on nemu,default enable

--- abstract-machine/am/include/am.h
+++ abstract-machine/am/include/am.h
@@ -44,35 +44,35 @@ extern "C" {
 #endif
 
 // ----------------------- TRM: Turing Machine -----------------------
-extern   Area        heap;
+extern   Area        heap; //可用的物理内存堆
 void     putch       (char ch);
-void     halt        (int code) __attribute__((__noreturn__));
+void     halt        (int code) __attribute__((__noreturn__)); //立即终止AM的运行并返回code
 
 // -------------------- IOE: Input/Output Devices --------------------
-bool     ioe_init    (void);
+bool     ioe_init    (void); //初始化IO设备
 void     ioe_read    (int reg, void *buf);
 void     ioe_write   (int reg, void *buf);
 #include "amdev.h"
 
 // ---------- CTE: Interrupt Handling and Context Switching ----------
 bool     cte_init    (Context *(*handler)(Event ev, Context *ctx));
-void     yield       (void);
-bool     ienabled    (void);
+void     yield       (void); //陷入内核执行
+bool     ienabled    (void); //读取/写入当前处理器的中断打开/屏蔽状态
 void     iset        (bool enable);
-Context *kcontext    (Area kstack, void (*entry)(void *), void *arg);
+Context *kcontext    (Area kstack, void (*entry)(void *), void *arg); //创建内核态运行的上下文
 
 // ----------------------- VME: Virtual Memory -----------------------
 bool     vme_init    (void *(*pgalloc)(int), void (*pgfree)(void *));
-void     protect     (AddrSpace *as);
+void     protect     (AddrSpace *as); //创建/销毁一个地址空间
 void     unprotect   (AddrSpace *as);
-void     map         (AddrSpace *as, void *vaddr, void *paddr, int prot);
-Context *ucontext    (AddrSpace *as, Area kstack, void *entry);
+void     map         (AddrSpace *as, void *vaddr, void *paddr, int prot); //对地址空间as建立vaddr->paddr的映射
+Context *ucontext    (AddrSpace *as, Area kstack, void *entry); //创建被保护的用户态进程上下文
 
 // ---------------------- MPE: Multi-Processing ----------------------
-bool     mpe_init    (void (*entry)());
-int      cpu_count   (void);
-int      cpu_current (void);
-int      atomic_xchg (int *addr, int newval);
+bool     mpe_init    (void (*entry)()); //启动多处理器
+int      cpu_count   (void);			//返回系统中的处理器个数
+int      cpu_current (void);			//当前处理器编号
+int      atomic_xchg (int *addr, int newval); //原子的交换内存中数值 写入新值 返回旧值
 
 #ifdef __cplusplus
 }
--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -1,24 +1,27 @@
 #include <am.h>
 #include <nemu.h>
 
-extern char _heap_start;
+extern char _heap_start; //外部符号 在链接器脚本scripts/linker.ld中指定
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, PMEM_END);
-static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
+Area heap = RANGE(&_heap_start, PMEM_END); //定义了一个[_heap_start,PMEM_END)的heap
+static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in scripts/platform/nemu.mk CFLAGS 通过insert-arg脚本将mainargs指定的内容替换到MAINARGS_PLACEHOLDER处
 
+//输出字符
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
 
+//结束程序运行
 void halt(int code) {
-  nemu_trap(code);
+  nemu_trap(code); //nemu_trap宏屏蔽了底层硬件的抽象
 
   // should not reach here
   while (1);
 }
 
+//初始化TRM
 void _trm_init() {
-  int ret = main(mainargs);
+  int ret = main(mainargs); //执行main函数
   halt(ret);
 }
