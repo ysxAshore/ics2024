243d46dd2e1bf2dbfa2ce265b679e7225165e078
ysxAshore <19819660125@163.com>
2025-06-07 23:02:23 +0800

    [add]implenmented the difftest reg comparison

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,18 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
+{
+  bool sign = true;
+  if (!difftest_check_reg("pc", pc, ref_r->pc, cpu.pc))
+    return false;
+  for (int i = 0; i < 32; ++i)
+  {
+    sign = difftest_check_reg(reg_name(i), pc, ref_r->gpr[i], cpu.gpr[i]);
+    if (!sign)
+      break;
+  }
+  return sign;
 }
 
 void isa_difftest_attach() {
